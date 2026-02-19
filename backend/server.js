246468df7c3865d51cfbe0cb936ec629fa63070b
Aditya Kumawat <adityakumawat2003@gmail.com>
2026-02-19 13:08:52 +0530

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -129,6 +129,8 @@ app.get("/leetcode/:username", async (req, res) => {
   }
 });
 /* ---------- START SERVER ---------- */
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
